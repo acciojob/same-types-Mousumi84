898a3bf406c8aeb29f5216b055f233eded4b5503
Mousumi Dsa <58068282+Mousumi84@users.noreply.github.com>
2024-02-18 03:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function isSameType(value1, value2) {
 	let ans;
+	console.log(typeof value1 + typeof value2);
   if((typeof value1== typeof value2) || (value1==NaN && value2==NaN))
 	 {
 	     ans="true";
@@ -15,3 +16,4 @@ function isSameType(value1, value2) {
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
